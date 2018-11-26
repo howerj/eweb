9e08b941e174513011dd5071a41625f58f5f2763
Richard James Howe <howe.r.j.89@gmail.com>
2018-11-26 21:09:55 +0000

    Initial Cleanup
    This is the initial project cleanup, and partially renaming/moving
    around of things, so the dweb webserver project can be to be more
    portable.
    
    * The formatting has been changed
    * A number of warnings have been fixed
    * Many files have been moved around in the project
    * Permissions fixed on files
    * Different makefile targets
    * A project outline/to-do has been added

---
+++ makefile
@@ -0,0 +1,18 @@
+CC=gcc
+CFLAGS=-Wall -Wextra -pedantic -O2 -std=gnu99
+LDFLAGS=-pthread
+
+.PHONY: all run clean
+
+all: eweb simple
+
+eweb: eweb.o dwebsvr.o
+
+simple: simple.o dwebsvr.o
+
+run: eweb
+	./eweb 1234
+
+clean:
+	rm -vf *.o *.exe eweb simple
+
---
+++ simple.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dwebsvr.h"
+
+static void simple_response(struct hitArgs *args, char *path, char *request_body, http_verb type) {
+	UNUSED(request_body);
+	UNUSED(type);
+	ok_200(args, "\nContent-Type: text/html",
+	       "<html><head><title>Test Page</title></head>"
+	       "<body><h1>Testing...</h1>This is a test response.</body>"
+	       "</html>", path);
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2 || !strcmp(argv[1], "-?")) {
+		printf("hint: simple [port number]\n");
+		exit(0);
+	}
+	dwebserver(atoi(argv[1]), &simple_response, NULL);
+	return 0;
+}
+
